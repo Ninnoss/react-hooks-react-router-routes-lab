f03567a3e3368fa2a70919e40c7d9ebc2dc211f6
Ninos Dinkha <ninos.dinkha97@gmail.com>
2023-05-01 23:03:47 +0300

    done

--- src/data.js
+++ src/data.js
@@ -1,56 +1,52 @@
 const movies = [
   {
-    title: "Doctor Strange",
+    title: 'Doctor Strange',
     time: 115,
-    genres: ["Action", "Adventure", "Fantasy"],
+    genres: ['Action', 'Adventure', 'Fantasy'],
   },
   {
-    title: "Trolls",
+    title: 'Trolls',
     time: 92,
-    genres: ["Animation", "Adventure", "Comedy", "Family", "Fantasy"],
+    genres: ['Animation', 'Adventure', 'Comedy', 'Family', 'Fantasy'],
   },
   {
-    title: "Jack Reacher: Never Go Back",
+    title: 'Jack Reacher: Never Go Back',
     time: 118,
-    genres: ["Action", "Adventure", "Crime", "Mystery", "Thriller"],
+    genres: ['Action', 'Adventure', 'Crime', 'Mystery', 'Thriller'],
   },
 ];
 
 const actors = [
   {
-    name: "Benedict Cumberbatch",
-    movies: ["Doctor Strange", "The Imitation Game", "Black Mass"],
+    name: 'Benedict Cumberbatch',
+    movies: ['Doctor Strange', 'The Imitation Game', 'Black Mass'],
   },
   {
-    name: "Justin Timberlake",
-    movies: ["Trolls", "Friends with Benefits", "The Social Network"],
+    name: 'Justin Timberlake',
+    movies: ['Trolls', 'Friends with Benefits', 'The Social Network'],
   },
   {
-    name: "Anna Kendrick",
-    movies: ["Pitch Perfect", "Into The Wood"],
+    name: 'Anna Kendrick',
+    movies: ['Pitch Perfect', 'Into The Wood'],
   },
   {
-    name: "Tom Cruise",
-    movies: [
-      "Jack Reacher: Never Go Back",
-      "Mission Impossible 4",
-      "War of the Worlds",
-    ],
+    name: 'Tom Cruise',
+    movies: ['Jack Reacher: Never Go Back', 'Mission Impossible 4', 'War of the Worlds'],
   },
 ];
 
 const directors = [
   {
-    name: "Scott Derrickson",
-    movies: ["Doctor Strange", "Sinister", "The Exorcism of Emily Rose"],
+    name: 'Scott Derrickson',
+    movies: ['Doctor Strange', 'Sinister', 'The Exorcism of Emily Rose'],
   },
   {
-    name: "Mike Mitchell",
-    movies: ["Trolls", "Alvin and the Chipmunks: Chipwrecked", "Sky High"],
+    name: 'Mike Mitchell',
+    movies: ['Trolls', 'Alvin and the Chipmunks: Chipwrecked', 'Sky High'],
   },
   {
-    name: "Edward Zwick",
-    movies: ["Jack Reacher: Never Go Back", "Blood Diamon", "The Siege"],
+    name: 'Edward Zwick',
+    movies: ['Jack Reacher: Never Go Back', 'Blood Diamon', 'The Siege'],
   },
 ];
 
